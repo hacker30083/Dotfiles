c89b91318077cb203f98242d8a5ba9bd4f8abc71
Kaspar Aun <114168113+hacker30083@users.noreply.github.com>
2025-06-02 15:28:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 #sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash iommu=off"' ~/grub.temp
 #mv ~/grub.temp /etc/default/grub
 
+sudo apt update 
+sudo apt upgrade
+sudo apt dist-upgrade
 sudo apt install build-essential neovim cmake git vim python3
 
 git clone https://github.com/hacker30083/Dotfiles ~/.config/
