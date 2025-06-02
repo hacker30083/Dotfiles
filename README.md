8adef7958d8c160605af79502081bd38d19c7498
Kaspar Aun <kaspar@Kaspar-MBP.lan>
2025-06-02 15:21:31 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dotfiles
+Here are the dotfiles for getting my configs
+
+##Installing
+
+'''sh
+    curl https://github.com/hacker30083/Dotfiles/setup.sh | sh
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#cp /etc/default/grub ~/grub.temp
+#sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash iommu=off"' ~/grub.temp
+#mv ~/grub.temp /etc/default/grub
+
+sudo apt install build-essential neovim cmake git vim python3
+
+git clone https://github.com/hacker30083/Dotfiles ~/.config/
+
